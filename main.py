f122aafefbe5e4e8f7ff83d712633aa3c5392e4d
hdsjejgh <aayanahmad08@hotmail.com>
2025-01-16 19:12:42 -0500

    comit

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import os
 
 
 def draw(filepath=None, scale = None):
-    #pyautogui.PAUSE = 0
+    pyautogui.PAUSE = 0.001
     if filepath is None or scale is None:
         filepath, scale = getImageInfo()
     img = Image.open(filepath)
     imageArray = np.array(img)
-    print("Drawing in 3 seconds... ")
-    sleep(3)
+    print("Drawing in 5 seconds... ")
+    sleep(50)
     mousePos = pyautogui.position()
     for ridx, row in enumerate(imageArray):
         for pidx, pixel in enumerate(row):
             average = sum(color for color in pixel.tolist())/(255*3)
-            if average>0.5:
+            if average<=0.5:
                 pyautogui.moveTo(mousePos[0]+pidx*scale,mousePos[1]+ridx*scale,0)
                 pyautogui.click()
                 pass
