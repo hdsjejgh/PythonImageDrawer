3e6c6f8c5283cc96f964b9c96bcad6e3a9a33356
hdsjejgh <aayanahmad08@hotmail.com>
2025-01-17 17:03:30 -0500

    Comments added

--- main.py
+++ main.py
@@ -1,52 +1,54 @@
-from PIL import Image
-import numpy as np
-from time import sleep
-import pyautogui
-import os
+from PIL import Image #To get the image
+import numpy as np #To represent image in array
+from time import sleep #To wait before beginning to draw
+import pyautogui #To move mouse
+import os #To verify image file
 
 
 def draw(filepath=None, scale = None, threshold=0.5, pause = 0.004):
-    def getImageInfo(default=1):
+    """
+    Draws image
+    :param filepath: Path of file
+    :param scale: How many pixels to jump each time (Defaults to 1)
+    :param threshold: Brightness level needed to constitute image as white (not draw it) (defaults to 0.5)
+    :param pause: Pause in seconds between movements (lower value move quicker, but can cause problems with the windows input buffer) (defaults to 0.004)
+    """
+    def getImageInfo(scale,default=1): #gets image info
         while not os.path.isfile(filepath := input("Enter file path: ").strip('"')):
             filepath = input("Enter file path: ").strip('"')
-        try:
-            scale = int(input(f"Enter Input Scale (default: {default}): "))
-        except:
-            print(f"Invalid scale. Default ({default}) selected")
+        if scale is not None:
             scale = default
         return (filepath, scale)
     pyautogui.PAUSE = pause
-    if filepath is None or scale is None:
-        filepath, scale = getImageInfo()
+    if filepath is None: #gets image info from console if not provided
+        filepath, scale = getImageInfo(scale)
+    if scale is None: #sets scale to 1 if None is provided
+        scale = 1
 
     img = Image.open(filepath)
-    imageArray = np.array(img)
+    imageArray = np.array(img) #represents image as 3d array [rows,pixels,RGB]
     print("Drawing in 3 seconds... ")
-    sleep(3)
-    mousePos = pyautogui.position()
-    for ridx, row in enumerate(imageArray):
-        width = len(row)
-        first = None
-        for pidx, pixel in enumerate(row):
-            average = sum(color for color in pixel.tolist())/(255*3)
+    sleep(3) #gives user time to switch to drawing application
+    mousePos = pyautogui.position() #sets top left mouse position
+    for ridx, row in enumerate(imageArray): #iterates over rows
+        width = len(row) #width of row
+        first = None #first black pixel's position in sequence of black pixels (used for dragging across large lines of black)
+        for pidx, pixel in enumerate(row): #iterates over pixels in row
+            average = sum(color for color in pixel.tolist())/(255*len(pixel)) #gets brightness value
             if average<=threshold: #if black pixel
-                if first is None:
+                if first is None: #if it is the first black pixel in row, or previous was white
                     first = (mousePos[0]+pidx*scale,mousePos[1]+ridx*scale,0)
-                if pidx == width-1:#if last pixel in row, do the row
+                if pidx == width-1:#if last pixel in row, draw from first black pixel in series to last pixel
                     pyautogui.moveTo(first[0],first[1])
-                    if (mousePos[0] + (pidx) * scale, mousePos[1] + (ridx) * scale) != first:
-                        pyautogui.dragTo(mousePos[0] + (pidx) * scale, mousePos[1] + (ridx) * scale,0)
+                    if (mousePos[0] + (pidx) * scale, mousePos[1] + (ridx) * scale) != first: #if the first pixel isnt the last pixel
+                        pyautogui.dragTo(mousePos[0] + (pidx) * scale, mousePos[1] + (ridx) * scale,0) #drag
                         first = None
                     else:
-                        pyautogui.click()
+                        pyautogui.click() #if first and last pixel are the same, just click
                         first = None
             else: #if white pixel
-                if first is not None:
+                if first is not None: #if white pixel is ending a series of black pixels
                     pyautogui.moveTo(first[0], first[1])
                     pyautogui.dragTo(mousePos[0]+(pidx-1)*scale,mousePos[1]+(ridx-1)*scale,0) #drags to previous pixel
                     first = None
-
-                #pyautogui.moveTo(mousePos[0]+pidx*scale,mousePos[1]+ridx*scale,0)
-                #pyautogui.click()
-                pass
     print("Drawing complete")
--- runner.py
+++ runner.py
@@ -1,3 +1,3 @@
 from main import draw
-if __name__ == '__main__':
+if __name__ == '__main__': #just runs it
     draw()
